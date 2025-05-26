e8032c936b4e0e29f53935d28e0fa19bdf02233b
andreabushnell <andreabushnell00@gmail.com>
2025-05-26 10:32:51 +0200

    Remove unused /sql

--- sql/desencadenadores/TR_ORGANIZADOR_DEL.sql
+++
@@ -1,9 +0,0 @@
-use Eventos_Sostenibles
-go 
-
-create or alter trigger TR_Organizador_DEL
-on ORGANIZADORES after delete
-as 
-    delete EVENTOS 
-    where Organizador=(select Nombre from deleted)
-go
--- sql/desencadenadores/TR_Usuario_DEL.sql
+++
@@ -1,9 +0,0 @@
-use Eventos_Sostenibles
-go
-
-create or alter trigger TR_Usuario_DEL
-on USUARIOS after delete
-as
-    delete INSCRIPCIONES 
-    where Usuario=(select Correo from Deleted)
-go
